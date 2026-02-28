fc3f5f61ff0a0f8227158b5b84200046cd17b0fd
protzzz <99717913+protzzz@users.noreply.github.com>
2026-02-28 15:57:22 +0100

    Add frontend path config to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,15 +2,22 @@ import express from "express";
 import dotenv from "dotenv";
 import searchGoogleRouter from "./routes/search.google";
 import searchSerperRouter from "./routes/search.serper";
+import path from "node:path";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const frontendPath = path.join(__dirname, "../../frontend");
 
 app.use(express.json());
 // app.use("/api", searchGoogleRouter);
 app.use("/api", searchSerperRouter);
+
+app.get("/", (req, res) => {
+  // res.send("Backend is running");
+  res.sendFile(path.join(frontendPath, "index.php"));
+});
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
