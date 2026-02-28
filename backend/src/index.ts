7bfa90da513c9d317feb1d696722b0ae2599dd06
protzzz <99717913+protzzz@users.noreply.github.com>
2026-02-28 15:07:01 +0100

    Inizio google search app

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import searchGoogleRouter from "./routes/search.google";
+import searchSerperRouter from "./routes/search.serper";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+// app.use("/api", searchGoogleRouter);
+app.use("/api", searchSerperRouter);
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
